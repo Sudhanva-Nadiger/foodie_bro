01e745ec9c217da8c42c8188e501bacb92532961
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-02-24 20:14:56 +0530

    adding steps and ingredients
    - styling

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,9 @@ class MyApp extends StatelessWidget {
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
               headline1: const TextStyle(
-                fontFamily: 'RobotoCondensed',
-                fontSize: 24,
-              ),
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               headline2: const TextStyle(
                 fontFamily: 'RobotoCondensed',
                 fontSize: 20,
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/constants/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
@@ -7,13 +9,103 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(mealId),
+          title: Text(selectedMeal.title),
         ),
-        body: Center(
-          child: Text("Meal info $mealId"),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SectionTitle('Ingredients'),
+              BuildContainer(
+                child: ListView.builder(
+                  itemCount: selectedMeal.ingredients.length,
+                  itemBuilder: (context, index) => Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(selectedMeal.ingredients[index]),
+                    ),
+                  ),
+                ),
+              ),
+              const SectionTitle('Steps'),
+              BuildContainer(
+                  child: ListView.builder(
+                itemCount: selectedMeal.steps.length,
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text(
+                        selectedMeal.steps[index],
+                      ),
+                    ),
+                    const Divider(),
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BuildContainer extends StatelessWidget {
+  const BuildContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 200,
+      width: 300,
+      child: child,
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  const SectionTitle(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: SizedBox(
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline1,
         ),
       ),
     );
